a9d4546dfb65f1afc23ede2af912cbbcc6b7de30
Ruargh <ruargh@gmail.com>
2018-12-06 11:26:47 +0200

    add FavoriteMovie component

---
+++ src/components/FavoriteMovie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FavoriteMovie extends Component {
+  render() {
+    return (
+      <div>
+        <p>Favorite movie!</p>
+      </div>
+    )
+  }
+}
+
+export default FavoriteMovie
